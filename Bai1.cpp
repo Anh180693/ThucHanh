80304e53c45e428ce3d94b0ecbce3da1ebd8ccdb
Anh180693 <anhltth2203042@fpt.edu.vn>
2022-06-04 10:32:15 +0700

    ThucHanh

--- Bai1.cpp
+++ Bai1.cpp
@@ -12,34 +12,44 @@ int main(){
 		}
 	}
 	while(n<100 || n>1000);
-	
+		
 	int k;
 	switch(k){
 		case 1:
 			printf("Mot");
+			break;
 			case 2:
 				printf("Hai");
+				break;
 				case 3:
 					printf("Ba");
+					break;
 					case 4:
 						printf("Bon");
+						break;
 						case 5:
 							printf("Nam");
+							break;
 							case 6:
 								printf("Sau");
+								break;
 								case 7:
 									printf("Bay");
+									break;
 									case 8:
 										printf("Tam");
+										break;
 										case 9:
 											printf("Chin");
+											break;
 											case 0:
 												printf("Khong");
+												break;
 	}
 	int i;
 	while(n!=0){
 		i=n%10;
 		n=n/10;
-		
+		switch(i);	
 	}
 }
